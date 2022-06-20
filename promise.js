0e480d424df87acf497fb84280afd74b66061828
hafidarsyah@gmail.com <hafidarsyah@gmail.com>
2022-06-20 18:39:13 +0700

    fix

--- promise.js
+++ promise.js
@@ -1,7 +1,27 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emotion) => {
+  const resultIXX = await promiseTheaterIXX();
+  const resultVGC = await promiseTheaterVGC();
+  let angry = 0,
+    notAngry = 0;
+
+  const YesOrNoAngry = (data) => {
+    data.forEach((item) => {
+      if (item.hasil === "marah") {
+        angry++;
+      } else {
+        notAngry++;
+      }
+    });
+  };
+
+  YesOrNoAngry(resultIXX);
+  YesOrNoAngry(resultVGC);
+
+  return emotion === "marah" ? angry : notAngry;
+};
 
 module.exports = {
   promiseOutput,
